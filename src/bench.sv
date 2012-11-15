f12f43bea6fd944289bfe0101c3bf8016e510794
lcr2131 <robinson.leonardc@gmail.com>
2012-11-15 13:24:43 -0500

    Make Bench Skeleton Completed - Top File Included

--- src/bench.sv
+++ src/bench.sv
@@ -3,19 +3,39 @@
 //Date Created:Saturday October 13, 2012
 //Date Modified:
 
-class data;
-   
-endclass // data
+class packet;
+   bit[31:0] instruction;
+endclass // packet
 
-class test;
+class i_inst;
+   bit [5:0] op  	     ;//opcode
+   bit [4:0] reg_src	     ;//rs
+   bit [4:0] reg_des;//rt
+   bit [15:0] immediate ;//imm
+   
+endclass // i_inst
+
+class r_inst;
+   bit [5:0]  op	;//Op_code
+   bit [4:0]  reg_src1	;//rs
+   bit [4:0]  reg_src2	;//rt
+   bit [4:0]  reg_dest	;//rd
+   bit [4:0]  shamt     ;//shamt
+   bit [5:0]  funct     ;//funct
+endclass // r_inst
+
+class reg_data;
+   bit [31:0] data;
+   
+endclass // reg_data
+
+class test;   
 endclass // test
 
 class issue_queue;
-   
 endclass // issue_queue
 
 class alu;
-
 endclass // alu
 
 class data_memory;
@@ -23,9 +43,9 @@ class data_memory;
 endclass // data_memory
 
 class register;
+   
 endclass // register
    
-
 class env;
 
    int cycle = 0;
@@ -57,44 +77,13 @@ class env;
 
 endclass // env
 
-
-/*
-class checker;
-   function bit int_check_result (int dut_value, int bench_value, bit verbose, string name); 
-      bit passed = (dut_value == bench_value);
-      if(passed) begin
-         if(verbose) $display("%t %s:\tpass %d\n", $realtime, name, dut_value);
-      end
-      else begin
-         $display("%t %s:\tfail", $realtime, name);
-         $display("----> dut value:   %d", dut_value);
-         $display("----> bench value: %d", bench_value);
-         $exit();
-      end
-      return passed;
-   endfunction
-
-   function bit bit_check_result(bit dut_value, bit bench_value, bit verbose, string name);
-      bit passed = (dut_value == bench_value);
-      if(passed) begin
-         if(verbose) $display("%t %s:\tpass %d", $realtime, name, dut_value);
-      end
-      else begin
-         $display("%t %s:\tfail", $realtime, name);
-         $display("----> dut value:   %d", dut_value);
-         $display("----> bench value: %d", bench_value);
-         $exit();
-      end
-      return passed;
-   endfunction
-endclass 
-*/
-
-program testbench ();
-   
-   data packet;
+program testbench (processor_interface.bench proc_tb);
+   packet instruction1;
+   packet instruction2;
+   i_inst i_type;
+   r_inst r_type;
+   reg_data data;
    test test;
-//   checker check;
    env env;
    int cycle;
 
@@ -103,17 +92,18 @@ program testbench ();
       env.cycle++;
       
       cycle = env.cycle;
-      packet = new();
-      packet.randomize();
-
+      instruction1 = new();
+      instruction2 = new();
+      
    endtask
 
    initial begin
       test = new();
-//      check = new();
-      packet = new();
+      instruction1 = new();
+      instruction2 = new();
+      
       env = new();
-      env.configure("config.txt");
+      env.configure("./src/config.txt");
 
       // warm up
       repeat (env.warmup_time) begin
@@ -123,7 +113,6 @@ program testbench ();
       // testing
       repeat (env.max_transactions) begin
          do_cycle();
-
 	 // only check this result if read_enable is set
       end			
    end
---
+++ src/top.sv
@@ -0,0 +1,21 @@
+//Author:
+//Name: top.sv
+//Date Created:
+//Date Modified:
+
+//`timescale 1ns/1ns
+
+module top();
+
+   bit clk = 0;
+   always #5 clk = ~clk;
+
+   initial $vcdpluson;
+
+   processor_interface IFC(clk);
+//   processor dut(IFC.dut); 
+   testbench bench(IFC.bench);
+   
+
+
+endmodule //top
