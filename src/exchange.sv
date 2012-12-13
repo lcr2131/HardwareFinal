3aa1581ade6626eb5748cb5c680fc65b99509be6
donald-e-pomeroy <dep2127@gmail.com>
2012-12-12 19:45:15 -0500

    Fixed some errors

--- src/exchange.sv
+++ src/exchange.sv
@@ -40,36 +40,37 @@ typedef union packed {
 } instr;
 
 function bit[31:0]  exchange(instr op);//new ones drop MSB for regs
-	if (op.R.opcode = 6'b000000 ) begin//add
+	if (op.R.opcode == 6'b000000 ) begin//add
   		op.proc_R.opcode = 4'b1000;
 		op.proc_R.rs = op.R.rs[3:0];
 		op.proc_R.rt = op.R.rt[3:0];
 		op.proc_R.rd = op.R.rd[3:0];
-		return op;
+		
 	end   
 	
-	if (op.I.opcode = 6'b000101 ) begin//bne
+	else if (op.I.opcode == 6'b000101 ) begin//bne
 		op.proc_I.opcode = 4'b0001;
 		op.proc_I.rs = op.I.rs[3:0];
 		op.proc_I.rt = op.I.rt[3:0];
 		op.proc_I.imm = op.I.imm;
-		return op
+		
 	end
 	
-	if (op.I.opcode = 6'b101011 ) begin//sw
+	else if (op.I.opcode == 6'b101011 ) begin//sw
 		op.proc_I.opcode = 4'b0100;
 		op.proc_I.rs = op.I.rs[3:0];
 		op.proc_I.rt = op.I.rt[3:0];
 		op.proc_I.imm = op.I.imm;
-		return op
+		
 	end
 
-	if (op.I.opcode = 6'b100011 ) begin//lw
+	else if (op.I.opcode == 6'b100011 ) begin//lw
 		op.proc_I.opcode = 4'b0010;
 		op.proc_I.rs = op.I.rs[3:0];
 		op.proc_I.rt = op.I.rt[3:0];
 		op.proc_I.imm = op.I.imm;
-		return op
+		
 	end
 
+	return op;
 endfunction // generateRandomInstruction
