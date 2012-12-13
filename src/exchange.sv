65032303c10ab5f3f8809896a61944ef063d020c
donald-e-pomeroy <dep2127@gmail.com>
2012-12-12 19:03:48 -0500

    Exchange function switches op codes

--- src/exchange.sv
+++ src/exchange.sv
@@ -22,9 +22,9 @@ typedef union packed {
    bit [3:0]  opcode;
    bit [3:0]  rs;
    bit [3:0]  rt;
-   // bit [] bid; Branch ID
-   bit [18:0] imm; 
-} proc_I;
+   bit [2:0] bid; //Branch ID
+   bit [15:0] imm; 
+} proc_I;//new ones
    
    struct     packed{
    bit 	      valid; 
@@ -33,47 +33,43 @@ typedef union packed {
    bit [3:0]  rt;
    bit [3:0]  rd;
    bit [2:0]  bid;
-   bit [4:0] shamt;
-   bit [5:0]  funct;
+   bit [11:0] remaining;//these don't matter
+   
 } proc_R;
    
 } instr;
 
-function bit[31:0]  exchange(instr op);
- if (op.R.opcode = 6'b000000 ) begin
-    op.proc_R.valid = 1'b0;
-    op.proc_R.opcode = 4'b1000;
-    op.proc_R.funct = 6'b100000;
-    op.proc_R.shamt = '0;
-    op.proc_R.rs = chooseRandomReadRegister();
-    op.proc_R.rt = chooseRandomReadRegister();
-    op.proc_R.rd = chooseRandomWriteRegister();
-    return op;
- end
-	 else if (op.proc_R.opcode = 6'b000000) begin
-	    op.I.opcode = 6'b000101;
-	    op.I.rs = chooseRandomReadRegister();
-	    op.I.rt = chooseRandomReadRegister();
-	    op.I.imm = $unsigned($random) & branch_mask;
-	    if ($unsigned($random) % 2) op.I.imm = -op.I.imm;
-	    return op;
-	 end
-	 else if (op.proc_R.opcode = 6'b000000) begin
-	    op.I.opcode = 6'b100011;
-	    op.I.rt = chooseRandomWriteRegister();
-	    op.I.rs = chooseRandomReadRegister();
-	    op.I.imm = $unsigned($random) & address_mask;
-//	    if ($unsigned($random) % 2) op.I.imm = -op.I.imm;
-	    return op;
-	 end
-	 else if (op.proc_R.opcode = 6'b000000) begin
-	    op.I.opcode = 6'b101011;
-	    op.I.rt = chooseRandomReadRegister();
-	    op.I.rs = chooseRandomReadRegister();
-	    op.I.imm = $unsigned($random) & address_mask;
-//	    if ($unsigned($random) % 2) op.I.imm = -op.I.imm;
-	    return op;
-	 end
-      end
-   
+function bit[31:0]  exchange(instr op);//new ones drop MSB for regs
+	if (op.R.opcode = 6'b000000 ) begin//add
+  		op.proc_R.opcode = 4'b1000;
+		op.proc_R.rs = op.R.rs[3:0];
+		op.proc_R.rt = op.R.rt[3:0];
+		op.proc_R.rd = op.R.rd[3:0];
+		return op;
+	end   
+	
+	if (op.I.opcode = 6'b000101 ) begin//bne
+		op.proc_I.opcode = 4'b0001;
+		op.proc_I.rs = op.I.rs[3:0];
+		op.proc_I.rt = op.I.rt[3:0];
+		op.proc_I.imm = op.I.imm;
+		return op
+	end
+	
+	if (op.I.opcode = 6'b101011 ) begin//sw
+		op.proc_I.opcode = 4'b0100;
+		op.proc_I.rs = op.I.rs[3:0];
+		op.proc_I.rt = op.I.rt[3:0];
+		op.proc_I.imm = op.I.imm;
+		return op
+	end
+
+	if (op.I.opcode = 6'b100011 ) begin//lw
+		op.proc_I.opcode = 4'b0010;
+		op.proc_I.rs = op.I.rs[3:0];
+		op.proc_I.rt = op.I.rt[3:0];
+		op.proc_I.imm = op.I.imm;
+		return op
+	end
+
 endfunction // generateRandomInstruction
