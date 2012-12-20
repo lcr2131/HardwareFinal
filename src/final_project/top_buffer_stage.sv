ef5047ed114d9166feb0abcc6daad6247f5ebfb9
donald-e-pomeroy <dep2127@gmail.com>
2012-12-19 20:22:26 -0500

    Connected top_buffer_stage interface to top buffer stage

--- src/final_project/top_buffer_stage.sv
+++ src/final_project/top_buffer_stage.sv
@@ -2,94 +2,13 @@
 //Date:		12/15/2012
 //Purpose:	Connecting alu_and_buffer, entry_select, shift_amount and buffer_new_addr_calculation
 
-module top_buffer_stage #(parameter des = 'd4, source1 = 'd4, source2 = 'd4, immediate = 'd5,
+module top_buffer_stage(top_buffer_stage_interface.top_buffer_stage_dut d); #(parameter des = 'd4, source1 = 'd4, source2 = 'd4, immediate = 'd5,
 				branch_id = 'd3, total_in = 4 + des + source1 + source2,
 				total_out = total_in + branch_id + 'd1 + immediate, 
 				reg_num = 'd16, register_width = 'd32, op = 'd4)
-(
-	input	clk,
-	input	rst,
 
-	input	[register_width-1:0]	in_1_s1_data,
-	input	[register_width-1:0]	in_1_s2_data,
-	input	[register_width-1:0]	in_2_s1_data,
-	input	[register_width-1:0]	in_2_s2_data,
-	input	[register_width-1:0]	in_3_s1_data,
-	input	[register_width-1:0]	in_3_s2_data,
-	input	[register_width-1:0]	in_4_s1_data,
-	input	[register_width-1:0]	in_4_s2_data,
 
-	input		[des-1:0]	in_1_des,
-	input		[des-1:0]	in_2_des,
-	input		[des-1:0]	in_3_des,
-	input		[des-1:0]	in_4_des,
 
-//	input		[2:0]		out_addr_1,
-//	input		[2:0]		out_addr_2,
-//	input		[2:0]		out_addr_3,
-//	input		[2:0]		out_addr_4,
-
-//	input		[4:0]		in_addr_1,
-//	input		[4:0]		in_addr_2,
-//	input		[4:0]		in_addr_3,
-//	input		[4:0]		in_addr_4,
-
-	input		[op-1:0]	in_1_op,
-	input		[op-1:0]	in_2_op,
-	input		[op-1:0]	in_3_op,
-	input		[op-1:0]	in_4_op,
-
-	input				in_1_vld,
-	input				in_2_vld,
-	input				in_3_vld,
-	input				in_4_vld,
-
-//	input		[2:0]		shift_amount_1,
-//	input		[2:0]		shift_amount_2,
-//	input		[2:0]		shift_amount_3,
-//	input		[2:0]		shift_amount_rest,
-
-	input		[immediate-1:0]	in_1_immediate,
-
-	input		[branch_id-1:0]	in_1_branch,
-	input		[branch_id-1:0]	in_2_branch,
-	input		[branch_id-1:0]	in_3_branch,
-	input		[branch_id-1:0]	in_4_branch,
-
-	input				mem_in_done,
-	input				flush_en,
-	input		[2:0]		flush_id,
-
-	output	reg	[des-1:0]	out_1_des,
-	output	reg	[des-1:0]	out_2_des,
-	output	reg	[des-1:0]	out_3_des,
-	output	reg	[des-1:0]	out_4_des,
-
-	output	reg [register_width-1:0]	out_1_data,
-	output	reg [register_width-1:0]	out_2_data,
-	output	reg [register_width-1:0]	out_3_data,
-	output	reg [register_width-1:0]	out_4_data,
-	
-	output	reg			out_1_vld,
-	output	reg			out_2_vld,
-	output	reg			out_3_vld,
-	output	reg			out_4_vld,
-
-	output	reg	[register_width-1:0]	out_1_mem_addr,
-
-	output	reg			out_load_flag,
-	output	reg			out_store_flag,
-
-	output	reg			buffer_full,
-	output	reg			buffer_empty,
-
-	output reg	[reg_num-1:0]	reg_out_to_raw_history
-);
-
-//	logic   ins_back_1;	
-//	logic   ins_back_2;	
-//	logic   ins_back_3;	
-//	logic   ins_back_4;
 
 	logic [2:0]	index_out_1;
 	logic [2:0]	index_out_2;
@@ -109,10 +28,10 @@ module top_buffer_stage #(parameter des = 'd4, source1 = 'd4, source2 = 'd4, imm
 
 entry_select	entry_select_buffer
 (
-	.ins_in_1(out_1_vld),		
-	.ins_in_2(out_2_vld),	 
-	.ins_in_3(out_3_vld),	
-	.ins_in_4(out_4_vld),	
+	.ins_in_1(d.out_1_vld),		
+	.ins_in_2(d.out_2_vld),	 
+	.ins_in_3(d.out_3_vld),	
+	.ins_in_4(d.out_4_vld),	
 	
 	.index_out_1,
 	.index_out_2,
@@ -122,10 +41,10 @@ entry_select	entry_select_buffer
 
 shift_amount	shift_amount_buffer
 (
-	.ins_in_1(out_1_vld),		
-	.ins_in_2(out_2_vld),	 
-	.ins_in_3(out_3_vld),	
-	.ins_in_4(out_4_vld),	
+	.ins_in_1(d.out_1_vld),		
+	.ins_in_2(d.out_2_vld),	 
+	.ins_in_3(d.out_3_vld),	
+	.ins_in_4(d.out_4_vld),	
 
 	.shift_entry1(shift_amount_1),	
 	.shift_entry2(shift_amount_2),
@@ -136,18 +55,18 @@ shift_amount	shift_amount_buffer
 
 buffer_ins_addr_calculation	buffer_ins_addr_calculation1
 (
-	.clk,
-	.rst,
+	.clk(d.clk),
+	.rst(d.rst),
 
-	.ins_in_1(out_1_vld),		
-	.ins_in_2(out_2_vld),	 
-	.ins_in_3(out_3_vld),	
-	.ins_in_4(out_4_vld),	
+	.ins_in_1(d.out_1_vld),		
+	.ins_in_2(d.out_2_vld),	 
+	.ins_in_3(d.out_3_vld),	
+	.ins_in_4(d.out_4_vld),	
 
-	.ins_new_1_vld(in_1_vld),
-	.ins_new_2_vld(in_2_vld),
-	.ins_new_3_vld(in_3_vld),
-	.ins_new_4_vld(in_4_vld),
+	.ins_new_1_vld(d.in_1_vld),
+	.ins_new_2_vld(d.in_2_vld),
+	.ins_new_3_vld(d.in_3_vld),
+	.ins_new_4_vld(d.in_4_vld),
 
 	.ins_new_1_addr,
 	.ins_new_2_addr,
@@ -157,22 +76,22 @@ buffer_ins_addr_calculation	buffer_ins_addr_calculation1
 
 alu_and_buffer	alu_and_buffer1
 (
-	.clk,
-	.rst,
+	.clk(d.clk),
+	.rst(d.rst),
 
-	.in_1_s1_data,
-	.in_1_s2_data,
-	.in_2_s1_data,
-	.in_2_s2_data,
-	.in_3_s1_data,
-	.in_3_s2_data,
-	.in_4_s1_data,
-	.in_4_s2_data,
+	.in_1_s1_data(d.in_1_s1_data),
+	.in_1_s2_data(d.in_1_s2_data),
+	.in_2_s1_data(d.in_2_s1_data),
+	.in_2_s2_data(d.in_2_s2_data),
+	.in_3_s1_data(d.in_3_s1_data),
+	.in_3_s2_data(d.in_3_s2_data),
+	.in_4_s1_data(d.in_4_s1_data),
+	.in_4_s2_data(d.in_4_s2_data),
 
-	.in_1_des,
-	.in_2_des,
-	.in_3_des,
-	.in_4_des,
+	.in_1_des(d.in_1_des),
+	.in_2_des(d.in_2_des),
+	.in_3_des(d.in_3_des),
+	.in_4_des(d.in_4_des),
 
 	.out_addr_1(index_out_1),
 	.out_addr_2(index_out_2),
@@ -184,57 +103,57 @@ alu_and_buffer	alu_and_buffer1
 	.in_addr_3(ins_new_3_addr),
 	.in_addr_4(ins_new_4_addr),
 
-	.in_1_op,
-	.in_2_op,
-	.in_3_op,
-	.in_4_op,
+	.in_1_op(d.in_1_op),
+	.in_2_op(d.in_2_op),
+	.in_3_op(d.in_3_op),
+	.in_4_op(d.in_4_op),
 
-	.in_1_vld,
-	.in_2_vld,
-	.in_3_vld,
-	.in_4_vld,
+	.in_1_vld(d.in_1_vld),
+	.in_2_vld(d.in_2_vld),
+	.in_3_vld(d.in_3_vld),
+	.in_4_vld(d.in_4_vld),
 
 	.shift_amount_1,
 	.shift_amount_2,
 	.shift_amount_3,
 	.shift_amount_rest,
 
-	.in_1_immediate,
+	.in_1_immediate(d.in_1_immediate),
 
-	.in_1_branch,
-	.in_2_branch,
-	.in_3_branch,
-	.in_4_branch,
+	.in_1_branch(d.in_1_branch),
+	.in_2_branch(d.in_2_branch),
+	.in_3_branch(d.in_3_branch),
+	.in_4_branch(d.in_4_branch),
 
-	.mem_in_done,
-	.flush_en,
-	.flush_id,
+	.mem_in_done(d.mem_in_done),
+	.flush_en(d.flush_en),
+	.flush_id(d.flush_id),
 
 	
-	.out_1_des,
-	.out_2_des,
-	.out_3_des,
-	.out_4_des,
+	.out_1_des(d.out_1_des),
+	.out_2_des(d.out_2_des),
+	.out_3_des(d.out_3_des),
+	.out_4_des(d.out_4_des),
 
-	.out_1_data,
-	.out_2_data,
-	.out_3_data,
-	.out_4_data,
+	.out_1_data(d.out_1_data),
+	.out_2_data(d.out_2_data),
+	.out_3_data(d.out_3_data),
+	.out_4_data(d.out_4_data),
 	
-	.out_1_vld,
-	.out_2_vld,
-	.out_3_vld,
-	.out_4_vld,
+	.out_1_vld(d.out_1_vld),
+	.out_2_vld(d.out_2_vld),
+	.out_3_vld(d.out_3_vld),
+	.out_4_vld(d.out_4_vld),
 
-	.out_1_mem_addr,
+	.out_1_mem_addr(d.out_1_mem_addr),
 
-	.out_load_flag,
-	.out_store_flag,
+	.out_load_flag(d.out_load_flag),
+	.out_store_flag(d.out_store_flag),
 
-	.buffer_full,
-	.buffer_empty,
+	.buffer_full(d.buffer_full),
+	.buffer_empty(d.buffer_empty),
 
-	.reg_out_to_raw_history
+	.reg_out_to_raw_history(d.reg_out_to_raw_history)
 );
 
 
